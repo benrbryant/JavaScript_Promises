88fa020c723ddc97945337a83ab9ed06a7556b6f
Ben <bbryant@truecoders.io>
2021-01-11 12:35:13 -0600

    Added solution to Answer branch.

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+let watching = new Promise((resolve, reject) => {
+  let userWatchingLiveStream = false;
+
+  if (userWatchingLiveStream) {
+    resolve("Thumbs up and Subscribe!");
+  } else {
+    reject("User left.");
+  }
+});
+
+watching
+  .then((message) => console.log(message))
+  .catch((error) => console.log(error));
