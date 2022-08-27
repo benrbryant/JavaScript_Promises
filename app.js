2c3eee5b6f1e9e9420cb4f0e83f2e5ec7e7c6ce7
Ben <brbryant2639@gmail.com>
2022-08-26 21:21:21 -0500

    UPDATE: fixed probability for potentialFail

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 /**
- * 
- * @returns A promise that is designed to resolve with a list of hobbits, or potentially fail with an failure object. The failure object includes a boolean success property and a string message property.  
+ *
+ * @returns A promise that is designed to resolve with a list of hobbits, or potentially fail with an failure object. The failure object includes a boolean success property and a string message property.
  */
 function getList() {
   return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        let potentialFail = Math.round(Math.random() * 100) < 10;
-        if (potentialFail) {
-          resolve(["Bilbo", "Frodo", "Sam", "Merry", "Pippin"])
-        } else {
-          reject({ success: false, message: "Failed to get list of hobbits." })
-        }
-        
-      }, 10)
-  })
+    setTimeout(() => {
+      let potentialFail = Math.round(Math.random() * 100) < 10;
+      if (potentialFail) {
+        reject({ success: false, message: "Failed to get list of hobbits." });
+      } else {
+        resolve(["Bilbo", "Frodo", "Sam", "Merry", "Pippin"]);
+      }
+    }, 10);
+  });
 }
 
 // TODO: Handle the resolved or rejected states of the promise
